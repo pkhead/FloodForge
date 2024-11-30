19c6594724c23179aa3c03153b805fbc228d6912
pkhead <chromosoze@gmail.com>
2024-11-30 10:49:13 -0600

    Update world.mk to work on Windows

--- build/world.mk
+++ build/world.mk
@@ -4,19 +4,28 @@
 # batch files/shell scripts and makefiles are difficult to use and to make platform/compiler-agnostic.
 #
 # TODO: couldn't figure out how to use static glfw from the libs folder. (usually, i just include glfw source as a submodule)
-#		so right now, it just uses glfw from the package manager.
+#		so right now, it just uses glfw from the package manager on Linux.
 
-CXX=c++
+CXX ?= c++
 
 # list of all cpp source files
 SOURCES=$(wildcard src/font/*.cpp) $(wildcard src/math/*.cpp) $(wildcard src/world/*.cpp) $(wildcard src/*.cpp)
+CPPFLAGS += --std=c++17
+INCLUDES = -I"include/"
 
-CPPFLAGS=--std=c++17
-INCLUDES=-I"include/" $(shell pkg-config --cflags glfw3) $(shell pkg-config --cflags gtk+-3.0)
-LIBS=$(shell pkg-config --static --libs glfw3) $(shell pkg-config --libs gtk+-3.0) -lGL
+ifeq ($(OS),Windows_NT)
+LIBS += lib/GLFW/libglfw3.a -lgdi32 -lopengl32 -luser32 -lcomdlg32 -lole32
+else
+# items to plug into pkg-config to find libs and includes
+REQPKGS += glfw3 gtk+-3.0
+endif
 
-# list of all .o files associated with source .cpp files
-OBJS=$(addprefix build/obj/, $(shell realpath --relative-to src $(SOURCES:%.cpp=%.o)))
+# find libs and includes from REQPKGS list. works if empty.
+INCLUDES += $(foreach pkg,$(REQPKGS),$(shell pkg-config --cflags $(pkg)))
+LIBS += $(foreach pkg,$(REQPKGS),$(shell pkg-config --libs $(pkg)))
+
+# collect list of all .o files associated with source .cpp files
+OBJS = $(addprefix build/obj/, $(shell realpath --relative-to src $(SOURCES:%.cpp=%.o)))
 
 # build an .o file from a .cpp file
 build/obj/%.o: src/%.cpp
@@ -25,10 +34,16 @@ build/obj/%.o: src/%.cpp
 	@$(CXX) -c $(CPPFLAGS) $(INCLUDES) $< -o $@
 
 # link all .o files to build FloodForge executable
-build/FloodForge: $(OBJS)
+build/FloodForge: pkgcheck $(OBJS)
 	@echo [LNK] $@
 	@$(CXX) -o $@ $(OBJS) $(LIBS)
 
-.PHONY: clean
+# throw error if any required packages are missing
+pkgcheck:
+ifneq ($(shell $(foreach pkg,$(REQPKGS),pkg-config --exists $(pkg) &&) echo 1),1)
+$(error Missing required packages: $(foreach pkg,$(REQPKGS),$(shell pkg-config --exists $(pkg) || echo $(pkg))))
+endif	
+
+.PHONY: clean pkgcheck
 clean:
 	rm -rf build/obj
